228d74a913db953747ece489dacc62908854077c
goDlin <godlin086@gmail.com>
2022-06-10 17:01:57 +0300

    time animation with timer

--- src/components/TimeView.js
+++ src/components/TimeView.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import dayjs from 'dayjs'
 import 'dayjs/locale/ru'
 import duration from 'dayjs/plugin/duration'
@@ -7,53 +7,26 @@ dayjs.locale('ru')
 dayjs.extend(duration)
 
 const TimeView = ({ duration, startTime }) => {
-    const allduration = duration + (startTime ? (new Date().getTime() - startTime) / 1000 : 0)
-    const dur = dayjs.duration(allduration, 'seconds')
+    const allduration = duration + (startTime ? (dayjs().valueOf() - startTime) / 1000 : 0)
+    const dur = dayjs.duration(allduration, 's')
     const hours = dur.hours() + 24 * dur.days()
 
-    const digits = [
-        Math.floor((hours / 10) % 10),
-        hours % 10,
-        Math.floor((dur.minutes() / 10) % 10),
-        dur.minutes() % 10,
-        Math.floor((dur.seconds() / 10) % 10),
-        dur.seconds() % 10
-    ]
+    const [time, setTime] = useState(dur)
 
-    const timerDigits = (first, count) => {
-        return [...Array(count).keys()].map((i) => {
-            let digit = i + first
-            if (digit >= count) digit -= count
-            return <div>{digit}</div>
-        })
-    }
+    useEffect(() => {
+        setTime(dur)
+        if (startTime) {
+            const timer = setInterval(() => {
+                setTime((prevTime) => prevTime.add(1, 's'))
+            }, 1000)
+        
+            return () => clearInterval(timer)
+        }
+    }, [allduration])
 
     return (
         <div className='time'>
-            {allduration > 0 &&
-            <div className={startTime ? 'clock anim' : 'clock'}>
-                <div className='digit hoursTen' style={startTime ? {animationDelay: -(digits[1] * 3600 + dur.minutes() * 60 + dur.seconds()) + 's'} : {}}>
-                    {startTime ? timerDigits(digits[0], 6) : digits[0]}
-                </div>
-                <div className='digit hours' style={startTime ? {animationDelay: -(dur.minutes() * 60 + dur.seconds()) + 's'} : {}}>
-                    {startTime ? timerDigits(digits[1], 10) : digits[1]}
-                </div>
-                <div>:</div>
-                <div className='digit minsTen' style={startTime ? {animationDelay: -(dur.seconds() + digits[3] * 60) + 's'} : {}}>
-                    {startTime ? timerDigits(digits[2], 6) : digits[2]}
-                </div>
-                <div className='digit mins' style={startTime ? {animationDelay: -dur.seconds() + 's'} : {}}>
-                    {startTime ? timerDigits(digits[3], 10) : digits[3]}
-                </div>
-                <div>:</div>
-                <div className='digit secsTen' style={startTime ? {animationDelay: -digits[5] + 's'} : {}}>
-                    {startTime ? timerDigits(digits[4], 6) : digits[4]}
-                </div>
-                <div className='digit secs'>
-                    {startTime ? timerDigits(digits[5], 10) : digits[5]}
-                </div>
-            </div>
-            }
+            {allduration > 0 && <div>{time.format('HH:mm:ss')}</div>}
         </div>
     )
 }
